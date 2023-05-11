1cf57b39e00645a4463ca9efc8b0e240f7ca24b2
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-05-11 16:46:01 -0300

    Adicionar classe EstiloTextoModel

---
+++ RotuloForm/Model/EstiloTextoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RotuloForm.Model
+{
+    public class EstiloTextoModel
+    {
+        public string NomeFonte { get; set; }
+        public float TamanhoFonte { get; set; }
+        public string Alinhamento { get; set; }
+        public bool Negrito { get; set; }
+        public bool Italico { get; set; }
+        public bool Sublinhado { get; set; }
+    }
+}
