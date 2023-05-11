699c681d3f82152e567b9b5376ed0b5c24e01d24
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-05-11 16:46:16 -0300

    Separar Classes

--- RotuloForm/FormPoc.cs
+++ RotuloForm/FormPoc.cs
@@ -1,8 +1,8 @@
 ﻿using Newtonsoft.Json;
+using RotuloForm.Model;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace RotuloForm
@@ -146,67 +146,6 @@ namespace RotuloForm
                     _ => StringAlignment.Near
                 }
             };
-        }
-
-        private class TextoModel
-        {
-            public string Texto { get; set; }
-            public int Width { get; set; }
-            public int Height { get; set; }
-            public string Cor { get; set; }
-            public float Angulo { get; set; }
-            public float X { get; set; }
-            public float Y { get; set; }
-            public EstiloTextoModel EstiloTexto { get; set; }
-
-            public Color ConverterCor(string cor)
-            {
-                if (VerificarCorHexadecimal(cor))
-                    return ColorTranslator.FromHtml(cor);
-
-                int[] valoresRgb = ExtrairValoresRGB(cor);
-                return Color.FromArgb(valoresRgb[0], valoresRgb[1], valoresRgb[2]);
-            }
-
-            private static bool VerificarCorHexadecimal(string cor)
-            {
-                if (string.IsNullOrEmpty(cor))
-                    return false;
-
-                return Regex.IsMatch(cor, @"^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$");
-            }
-
-            private static int[] ExtrairValoresRGB(string cor)
-            {
-                if (string.IsNullOrEmpty(cor))
-                    throw new ArgumentException("A cor não pode ser nula ou vazia.");
-
-                string[] valoresRgb = cor.Replace("rgb(", "").Replace(")", "").Split(',');
-
-                if (valoresRgb.Length != 3)
-                    throw new ArgumentException("A string de cor para RGB não está no formato correto.");
-
-                int[] valores = new int[3];
-
-                for (int i = 0; i < 3; i++)
-                {
-                    if (!int.TryParse(valoresRgb[i].Trim(), out valores[i]) || valores[i] < 0 || valores[i] > 255)
-                        throw new ArgumentException("A string de cor não está no formato correto.");
-                }
-
-                return valores;
-            }
-
-        }
-
-        private class EstiloTextoModel
-        {
-            public string NomeFonte { get; set; }
-            public float TamanhoFonte { get; set; }
-            public string Alinhamento { get; set; }
-            public bool Negrito { get; set; }
-            public bool Italico { get; set; }
-            public bool Sublinhado { get; set; }
         }
 
     }
