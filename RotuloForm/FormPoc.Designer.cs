678c5e058e4a64576664bd4f172f3f0c49f146e1
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2023-05-11 14:17:03 -0300

    Add project files.

---
+++ RotuloForm/FormPoc.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace RotuloForm
+{
+    partial class FormRotulo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.picImg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(673, 415);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(95, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar Imagem";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // picImg
+            // 
+            this.picImg.Location = new System.Drawing.Point(12, 12);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(632, 379);
+            this.picImg.TabIndex = 1;
+            this.picImg.TabStop = false;
+            // 
+            // FormRotulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImg);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "FormRotulo";
+            this.Text = "Teste DrawString";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.PictureBox picImg;
+    }
+}
+
---
+++ RotuloForm/FormPoc.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RotuloForm
+{
+    public partial class FormRotulo : Form
+    {
+
+        public FormRotulo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            var json = @"
+                        [
+                            {
+                                ""Texto"": ""Texto 1"",
+                                ""Width"": 44,
+                                ""Height"": 29,
+                                ""Cor"": ""#ff0000"",
+                                ""Angulo"": 270.0,
+                                ""X"": 10.0,
+                                ""Y"": 50.0
+                            },
+                            {
+                                ""Texto"": ""Texto 2"",
+                                ""Width"": 38,
+                                ""Height"": 14,
+                                ""Cor"": ""#333333"",
+                                ""Angulo"": 90.0,
+                                ""X"": 100.0,
+                                ""Y"": 200.0
+                            },
+                            {
+                                ""Texto"": ""Texto 3"",
+                                ""Width"": 60,
+                                ""Height"": 20,
+                                ""Cor"": ""#1e66e3"",
+                                ""Angulo"": 180.0,
+                                ""X"": 500.0,
+                                ""Y"": 300.0
+                            },
+                            {
+                                ""Texto"": ""Schin"",
+                                ""Width"": 60,
+                                ""Height"": 20,
+                                ""Cor"": ""#f705ff"",
+                                ""Angulo"": 315.0,
+                                ""X"": 150.0,
+                                ""Y"": 150.0
+                            }
+                        ]";
+
+            List<TextoModel> textos = JsonConvert.DeserializeObject<List<TextoModel>>(json);
+
+            Bitmap bitmap = new(picImg.Width, picImg.Height);
+            Graphics desenhador = Graphics.FromImage(bitmap);
+
+            desenhador.Clear(Color.White);
+
+            Font fonte = new("Arial", 14, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            // TODO: pensar para pegar fonte no foreach
+            // TODO: fazer map de rgb para hexadecimal - ConverterRgbParaHexadecimal()
+
+            foreach (var texto in textos)
+            {
+                SolidBrush brush = new(ColorTranslator.FromHtml(texto.Cor));
+
+                desenhador.TranslateTransform(texto.X, texto.Y);
+
+                desenhador.RotateTransform(texto.Angulo);
+
+                desenhador.DrawString(texto.Texto, fonte, brush, new PointF(0, 0));
+
+                desenhador.RotateTransform(-texto.Angulo);
+                desenhador.TranslateTransform(-texto.X, -texto.Y);
+            }
+
+            picImg.Image = bitmap;
+        }
+
+        private class TextoModel
+        {
+            public string Texto { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public string Cor { get; set; }
+            public float Angulo { get; set; }
+            public float X { get; set; }
+            public float Y { get; set; }
+        }
+
+    }
+}
